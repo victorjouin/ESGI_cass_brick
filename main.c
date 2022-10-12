ef126a9957a8d95c53d0815183220772492cca21
victorjouin <jouinvictor1@gmail.com>
2022-10-12 13:17:05 -0700

    can apss argument for file

--- main.c
+++ main.c
@@ -2,8 +2,10 @@
 
 typedef struct tab tab;
 
-void main()
+void main(int *argc, char **argv)
 {
+    printf("%s \n", argv[1]);
+    // FILE * f = fopen();
     int width = 50;
     int height = 8;
     tab *tabs = initTab(width, height);
