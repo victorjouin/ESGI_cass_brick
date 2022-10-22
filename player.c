32b17d3bc9086519d90c37997757cc998a9e1f47
victorjouin <jouinvictor1@gmail.com>
2022-10-22 04:27:48 -0700

    can move from a side to another

--- player.c
+++ player.c
@@ -55,20 +55,30 @@ void movePlayer(player1 *p1, tab *tabs, win *displays, bonus *b1)
             if (c == 'z' && tabs->tableau[(p1->x) - 1][(p1->y)] == ' ')
             {
                 if (tabs->tableau[(p1->x)][(p1->y)] == 'P')
+                {
                     tabs->tableau[(p1->x)][(p1->y)] = ' ';
-                tabs->tableau[(p1->x) - 1][(p1->y)] = 'P';
-                p1->x -= 1;
+                    tabs->tableau[(p1->x) - 1][(p1->y)] = 'P';
+                    p1->x -= 1;
+                }
                 display(tabs, displays);
                 turn++;
                 bombChecker(tabs, b1);
             }
 
-            if (c == 'q' && tabs->tableau[(p1->x)][(p1->y) - 1] == ' ')
+            if (c == 'q' && tabs->tableau[(p1->x)][(p1->y) - 1] == ' ' || p1->y - 1 <= 0)
             {
                 if (tabs->tableau[(p1->x)][(p1->y)] == 'P')
                     tabs->tableau[(p1->x)][(p1->y)] = ' ';
-                tabs->tableau[(p1->x)][(p1->y) - 1] = 'P';
-                p1->y -= 1;
+                if (p1->y - 1 == 0)
+                {
+                    tabs->tableau[(p1->x)][(tabs->x) - 1] = 'P';
+                    p1->y = tabs->x - 1;
+                }
+                else
+                {
+                    tabs->tableau[(p1->x)][(p1->y) - 1] = 'P';
+                    p1->y -= 1;
+                }
                 display(tabs, displays);
                 turn++;
                 bombChecker(tabs, b1);
@@ -77,20 +87,31 @@ void movePlayer(player1 *p1, tab *tabs, win *displays, bonus *b1)
             if (c == 's' && tabs->tableau[(p1->x) + 1][(p1->y)] == ' ')
             {
                 if (tabs->tableau[(p1->x)][(p1->y)] == 'P')
+                {
                     tabs->tableau[(p1->x)][(p1->y)] = ' ';
-                tabs->tableau[(p1->x) + 1][(p1->y)] = 'P';
-                p1->x += 1;
+                    tabs->tableau[(p1->x) + 1][(p1->y)] = 'P';
+                    p1->x += 1;
+                }
                 display(tabs, displays);
                 turn++;
                 bombChecker(tabs, b1);
             }
 
-            if (c == 'd' && tabs->tableau[(p1->x)][(p1->y) + 1] == ' ')
+            if (c == 'd' && tabs->tableau[(p1->x)][(p1->y) + 1] == ' ' || p1->y == tabs->x)
             {
                 if (tabs->tableau[(p1->x)][(p1->y)] == 'P')
                     tabs->tableau[(p1->x)][(p1->y)] = ' ';
-                tabs->tableau[(p1->x)][(p1->y) + 1] = 'P';
-                p1->y += 1;
+                if (p1->y == tabs->x)
+                {
+                    tabs->tableau[(p1->x)][2] = 'P';
+                    p1->y = 2;
+                }
+                else
+                {
+                    tabs->tableau[(p1->x)][(p1->y) + 1] = 'P';
+                    p1->y += 1;
+                }
+
                 display(tabs, displays);
                 turn++;
                 bombChecker(tabs, b1);
@@ -103,7 +124,7 @@ void movePlayer(player1 *p1, tab *tabs, win *displays, bonus *b1)
             display(tabs, displays);
         }
 
-        mvwprintw(displays->window, 0, 0, "joueur 1 ton tour est fini a toi joueur 2 !");
+        mvwprintw(displays->window, 0, 0, "joueur 1 ton tour est fini a toi joueur 2 ! y= %d", p1->y);
     }
 }
 
@@ -129,12 +150,20 @@ void movePlayer2(player1 *p1, tab *tabs, win *displays, bonus *b1)
                 bombChecker(tabs, b1);
             }
 
-            if (c == 'q' && tabs->tableau[(p1->x)][(p1->y) - 1] == ' ')
+            if (c == 'q' && tabs->tableau[(p1->x)][(p1->y) - 1] == ' ' || p1->y - 1 <= 0)
             {
                 if (tabs->tableau[(p1->x)][(p1->y)] == 'T')
                     tabs->tableau[(p1->x)][(p1->y)] = ' ';
-                tabs->tableau[(p1->x)][(p1->y) - 1] = 'T';
-                p1->y -= 1;
+                if (p1->y - 1 == 0)
+                {
+                    tabs->tableau[(p1->x)][(tabs->x) - 1] = 'T';
+                    p1->y = tabs->x - 1;
+                }
+                else
+                {
+                    tabs->tableau[(p1->x)][(p1->y) - 1] = 'T';
+                    p1->y -= 1;
+                }
                 display(tabs, displays);
                 turn++;
                 bombChecker(tabs, b1);
@@ -151,12 +180,21 @@ void movePlayer2(player1 *p1, tab *tabs, win *displays, bonus *b1)
                 bombChecker(tabs, b1);
             }
 
-            if (c == 'd' && tabs->tableau[(p1->x)][(p1->y) + 1] == ' ')
+            if (c == 'd' && tabs->tableau[(p1->x)][(p1->y) + 1] == ' ' || p1->y == tabs->x)
             {
                 if (tabs->tableau[(p1->x)][(p1->y)] == 'T')
                     tabs->tableau[(p1->x)][(p1->y)] = ' ';
-                tabs->tableau[(p1->x)][(p1->y) + 1] = 'T';
-                p1->y += 1;
+                if (p1->y == tabs->x)
+                {
+                    tabs->tableau[(p1->x)][2] = 'T';
+                    p1->y = 2;
+                }
+                else
+                {
+                    tabs->tableau[(p1->x)][(p1->y) + 1] = 'T';
+                    p1->y += 1;
+                }
+
                 display(tabs, displays);
                 turn++;
                 bombChecker(tabs, b1);
@@ -167,6 +205,7 @@ void movePlayer2(player1 *p1, tab *tabs, win *displays, bonus *b1)
                 bombChecker(tabs, b1);
             }
         }
+        mvwprintw(displays->window, 0, 0, "PASSE x=%d y=%d", p1->x, p1->y);
         mvwprintw(displays->window, 0, 0, "joueur 2 ton tour est fini a toi joueur 1 !");
     }
 }
